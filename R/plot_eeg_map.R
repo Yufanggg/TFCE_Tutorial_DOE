1c9bc1fa361d27f6072c4116fc5a0359b7f11874
Yufanggg <wangalisa2017@gmail.com>
2026-07-16 17:13:25 +0800

    formating

---
+++ R/plot_eeg_map.R
@@ -0,0 +1,26 @@
+## ==========================================================
+## Helper plotting function
+## ==========================================================
+
+plot_eeg_map <- function(mat, title, times, nChan) {
+  
+  fields::image.plot(
+    x = times,
+    y = seq_len(nChan),
+    z = t(mat),
+    xlab = "Time (ms)",
+    ylab = "Channel",
+    main = title,
+    axes = FALSE
+  )
+  
+  axis(1, at = seq(-200, 800, by = 200))
+  axis(
+    2,
+    at = seq_len(nChan),
+    labels = channels,
+    las = 2,
+    cex.axis = 0.6
+  )
+  box()
+}
